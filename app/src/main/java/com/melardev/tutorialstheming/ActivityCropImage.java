7c6a7a0d96c1d742565809d088cb90f85752a67f
melardev <melardev@gmail.com>
2017-05-28 03:07:10 +0200

    CropImage Demos

--- app/src/main/java/com/melardev/tutorialstheming/ActivityCropImage.java
+++ app/src/main/java/com/melardev/tutorialstheming/ActivityCropImage.java
@@ -12,9 +12,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ActivityCropImage extends AppCompatActivity {
     private static final int MAX_HEIGHT = 300;
@@ -22,7 +27,8 @@ public class ActivityCropImage extends AppCompatActivity {
     private static final int RC_GET_IMG = 100;
     private ImageView imgCrop;
 
-    //Ucrop
+    //Ucrop https://github.com/Yalantis/uCrop
+    //Cropper
 
 
     @Override
@@ -65,25 +71,85 @@ public class ActivityCropImage extends AppCompatActivity {
             Toast.makeText(this, data.getDataString(), Toast.LENGTH_SHORT).show();
             Uri newUri = getImageContentUri(this, new File(data.getData().getPath()));
             Toast.makeText(this, newUri.getPath(), Toast.LENGTH_SHORT).show();
-
         }
 
-        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+        if (requestCode == UCrop.REQUEST_CROP) {
             final Uri resultUri = UCrop.getOutput(data);
             Toast.makeText(this, resultUri.getPath(), Toast.LENGTH_SHORT).show();
+            imgCrop.setImageDrawable(null); //otherwise the imageView does not refresh after the first try
             imgCrop.setImageURI(resultUri);
+            File tempCropped = new File(getCacheDir(), "tempImgCropped.png");
+            tempCropped.delete();
         } else if (resultCode == UCrop.RESULT_ERROR) {
             final Throwable cropError = UCrop.getError(data);
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                imgCrop.setImageDrawable(null); //otherwise the imageView does not refresh after the first try
+                imgCrop.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
     public void uCropDemo(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*");
-        startActivityForResult(intent, RC_GET_IMG);
+        //    Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*");
+        //      startActivityForResult(intent, RC_GET_IMG);
+
+        try {
+            InputStream is = getAssets().open("imgForCrop.JPG"); //Case sensitive!!!
+            File temp = new File(getCacheDir(), "tempImgOriginal.png");
+            FileOutputStream fos = new FileOutputStream(temp);
+            File tempCropped = new File(getCacheDir(), "tempImgCropped.png");
+            byte[] buffer = new byte[1024];
+            int size;
+            while ((size = is.read(buffer)) != -1)
+                fos.write(buffer, 0, size);
+            fos.close();
+            Uri sourceUri = Uri.fromFile(temp);
+            Uri destinationUri = Uri.fromFile(tempCropped);
+            UCrop.of(sourceUri, destinationUri)
+                    //.withAspectRatio(3, 2)
+                    //.withMaxResultSize(MAX_WIDTH, MAX_HEIGHT)
+                    .start(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void cropperDemo(View view) {
+        InputStream is = null; //Case sensitive!!!
+        try {
+            is = getAssets().open("imgForCrop.JPG");
 
+            File temp = new File(getCacheDir(), "tempImgOriginal.png");
+            FileOutputStream fos = new FileOutputStream(temp);
+            File tempCropped = new File(getCacheDir(), "tempImgCropped.png");
+            byte[] buffer = new byte[1024];
+            int size;
+            while ((size = is.read(buffer)) != -1)
+                fos.write(buffer, 0, size);
+            fos.close();
+
+            Uri sourceUri = Uri.fromFile(temp);
+            CropImage.activity(sourceUri)
+                    .setActivityTitle("Cropper Demo")
+                    .setGuidelines(CropImageView.Guidelines.ON).start(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*
+// start cropping activity for pre-acquired image saved on the device
+        CropImage.activity(imageUri)
+                .start(this);*/
+/*
+    // for fragment (DO NOT use `getActivity()`)
+        CropImage.activity()
+                .start(getContext(), this); */
     }
 
 }
